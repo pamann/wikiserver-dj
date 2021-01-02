ab4843b933376056f56a6e580386612995035d20
Pam <beardsell.pamela@gmail.com>
2021-01-02 11:44:11 -0500

    moved nav_options to emusii model

--- django_server/emusii/admin.py
+++ django_server/emusii/admin.py
@@ -5,7 +5,6 @@ from .models import emusii, graph, emoji
 # class emusiiAdmin(admin.ModelAdmin):
 #     list_display= ("title", "emoji")
 
-# Register your models here.
 admin.site.register(emusii)
 admin.site.register(graph)
 admin.site.register(emoji)
--- django_server/emusii/models.py
+++ django_server/emusii/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class emoji(models.Model):
-    title = models.CharField(max_length=50)
-    word = models.CharField(max_length=50)
+    title = models.CharField(max_length=16)
+    word = models.CharField(max_length=64)
         
     def _str_(self):
         return self.title
@@ -10,6 +10,8 @@ class emoji(models.Model):
 class emusii(models.Model):
     title = models.CharField(max_length=120)
     emoji = models.ForeignKey(emoji, on_delete=models.CASCADE)
+    channel = models.CharField(max_length=120)
+    nav_options = models.ManyToManyField(emoji)
 
     def _str_(self):
         return self.title
@@ -17,7 +19,6 @@ class emusii(models.Model):
 class graph(models.Model):
     active = models.ForeignKey(emusii, on_delete=models.CASCADE, related_name="active")
     nodes = models.ManyToManyField(emusii, related_name="nodes")
-    nav_options = models.ManyToManyField(emoji)
 
     def _str_(self):
         return self.active
