173a7f94236459899e3235fd3d5347c712c020c9
Drew Tada <drew@builders-lt10.myfiosgateway.com>
2021-01-11 17:09:34 -0500

    filtering works now

--- django_server/emusii/views.py
+++ django_server/emusii/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import nodeSerializer, emojiSerializer
 from .models import node, emoji
 
 class nodeView(viewsets.ModelViewSet):
     queryset = node.objects.all()
     serializer_class = nodeSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields =['_emoji']
+
 
 class emojiView(viewsets.ModelViewSet):
     queryset = emoji.objects.all()
     serializer_class = emojiSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields =['title']
